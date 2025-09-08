9343ef76182224c5bcce9a55cc55e1b4982ae42e
ekozmdev <130561749+ekozmdev@users.noreply.github.com>
2025-09-08 13:24:49 +0900

    section3-22

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+const MainLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="flex h-screen">
+        <div className="bg-indigo-300">サイドメニュー</div>
+        <div className="bg-red-300 flex-1 overflow-auto">{ children }</div>
+    </div>
+  )
+}
+
+export default MainLayout;
