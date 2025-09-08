35451a88ef10c2651dc826e6e5970980862ff956
ekozmdev <130561749+ekozmdev@users.noreply.github.com>
2025-09-08 15:32:58 +0900

    fix: いらない改行を削除

--- src/app/(main)/new/page.tsx
+++ src/app/(main)/new/page.tsx
@@ -5,7 +5,6 @@ const NewTaskPage = () => {
     <div className="flex flex-col justify-center py-20">
         <h2 className="text-center text-2xl font-bold">Create New Task</h2>
         <NewTaskForm />
-        
     </div>
   )
 }
