c666da2e56949236ce1c6999bcb381ca86bde5f2
ekozmdev <130561749+ekozmdev@users.noreply.github.com>
2025-09-09 18:03:48 +0900

    section: 4-42

---
+++ src/app/api/tasks/[id]/route.ts
@@ -0,0 +1,30 @@
+// routeの方にも[]で囲った名前のフォルダを使うことができる
+// 動的ルーティング
+
+import { TaskModel } from "@/models/task";
+import { connectDb } from "@/utils/database";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (_: NextRequest, { params } : { params: { id: string } }) => {
+    try {
+        await connectDb()
+        const task = await TaskModel.findById(params.id)
+
+        if (!task) {
+            return NextResponse.json(
+                { message: "タスクが存在しません"},
+                { status: 404 }
+            )
+        }
+
+        return NextResponse.json({message: "タスク取得成功", task})
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json(
+            { message: "タスク取得失敗"},
+            { status: 500 }
+        )
+    }
+}
+
+export const dynamic = "force-dynamic"
